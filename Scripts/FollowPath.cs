962380d7a811a8500da15a75c03186ee9b49cc88
CamilleStocq <95492172+CamilleStocq@users.noreply.github.com>
2025-09-03 15:37:02 +0200

    ending script followPath

--- Scripts/FollowPath.cs
+++ Scripts/FollowPath.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class FollowPath : MonoBehaviour
@@ -8,18 +9,54 @@ public class FollowPath : MonoBehaviour
     [SerializeField] private float travelTime;
 
     private float startTime;
+    private Coroutine coroutine;
+    private bool coroutineRunning;
+
+    IEnumerator MoveBetweenPonits()
+    {
+        coroutineRunning = true;
+        startTime = Time.time; // commence le mouvement
+        transform.position = startPoint.position; // deplacement de l'objet
+        float t;
+
+        while (Time.time - startTime < travelTime)
+        {
+            t = (Time.time - startTime) / travelTime;
+            transform.position = Vector3.Lerp(startPoint.position, endPoint.position, t);
+            // Lerp() = onse met à un point de depart et de fin et bien on a le pourcentage parcouru
+
+            yield return true;
+        }
+
+        transform.position = endPoint.position; // pile poil à la position final
+        coroutineRunning = false;
+
+
+    }
 
     void Start()
     {
-        transform.position = startPoint.position; // deplacement de l'objet
-        startTime = Time.time;
+       coroutine = StartCoroutine(MoveBetweenPonits());
     }
 
     void Update()
     {
-        float t = (Time.time - startTime) / travelTime;
-        // mettre l'objet à une certaine distance des deux points en fonction du temps
-        // Lerp() = onse met à un point de depart et de fin et bien on a le pourcentage parcouru
-        transform.position = Vector3.Lerp(startPoint.position, endPoint.position, t);
+        // float t = (Time.time - startTime) / travelTime;
+        // // mettre l'objet à une certaine distance des deux points en fonction du temps
+        // transform.position = Vector3.Lerp(startPoint.position, endPoint.position, t); 
+
+        // if (transform.position == endPoint.position)
+        // {
+        // }
+
+        if (!coroutineRunning) // si elle ne tourne pas
+        {
+            // on part dans l'autre sens
+            Transform tmp = endPoint;
+            endPoint = startPoint;
+            startPoint = tmp;
+            coroutine = StartCoroutine(MoveBetweenPonits()); 
+
+        }
     }
 }
